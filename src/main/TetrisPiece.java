464f0b56b5fb62317c102a92383db75239c1e431
Smqkmj <gotenks.ssj499@gmail.com>
2022-05-02 20:49:40 -0400

    done with Tertispiece.java

---
+++ src/main/TetrisPiece.java
@@ -0,0 +1,30 @@
+package main;
+
+public class TetrisPiece {
+public int distance;
+public Direction direction;
+public Tetromino parent;
+public int x,y;
+
+public void setParent(Tetromino parent) {
+	this.parent=parent;
+	x=parent.x+distance*direction.x;
+	y=parent.y+distance*direction.y;
+}
+
+public void setDirection(Direction direction) {
+	this.direction=direction;
+	x=parent.x+distance*direction.x;
+	y=parent.y+distance*direction.y;
+}
+
+public TetrisPiece(int distance, Direction direction) {
+	this.distance=distance;
+	this.direction=direction;
+}
+
+public TetrisPiece copy() {
+	return new TetrisPiece(distance, direction)
+}
+
+}
